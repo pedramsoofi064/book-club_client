409d7d7c14d621576cf90332f9ab2414301cc2a9
free4lancer247 <@example.com>
2023-12-11 00:34:00 +0330

    feat: footer

---
+++ src/components/common/icons/telegram-icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_111_2774)">
+      <path
+        d="M11.795 16.9061L9.72995 18.9151C9.37195 19.2631 8.77295 19.1091 8.62795 18.6311L7.25195 14.1011"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M14.1038 10.927C14.1038 10.927 11.5148 13.263 10.1868 14.462C9.78983 14.82 9.82283 15.45 10.2528 15.767L15.6308 19.74C16.1598 20.131 16.9158 19.844 17.0528 19.2L19.6938 6.75299C19.8218 6.15099 19.2308 5.64799 18.6568 5.86899L3.14183 11.853C2.68483 12.029 2.70683 12.682 3.17383 12.828L7.25083 14.1"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_111_2774">
+        <rect width="24" height="24" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
--- src/components/common/the-footer/the-footer.vue
+++ src/components/common/the-footer/the-footer.vue
@@ -1,22 +1,150 @@
 <template>
   <footer class="footer">
-    <div class="footer__main container"></div>
+    <div class="footer__main container">
+      <div class="footer__socials">
+        <h3 class="footer__title">شبکه‌های اجتماعی ما</h3>
+        <div class="footer__social-items">
+          <div class="social">
+            <icon-loader name="youtube" />
+          </div>
+          <div class="social">
+            <icon-loader name="telegram" />
+          </div>
+          <div class="social">
+            <icon-loader name="instagram" />
+          </div>
+        </div>
+      </div>
+      <div class="footer__hero hero">
+        <h1 class="hero__headline">بوک‌کلاب ریحان</h1>
+        <h1 class="hero__sub-headline">ما اینجا هستیم</h1>
+        <div class="hero__actions">
+          <base-button class="hero__btn">به جمع ما بپیوند</base-button>
+          <base-button class="hero__btn" variant="outline"
+            >مشاوره رایگان</base-button
+          >
+        </div>
+      </div>
+      <div class="footer__groups" v-for="(item, index) in items" :key="index">
+        <div>
+          <h3 class="footer__title">بوک کلاب‌ {{ item.title }}</h3>
+          <div
+            class="footer__group"
+            v-for="(i, index) in item.groups"
+            :key="index"
+          >
+            {{ i }}
+          </div>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import iconLoader from "../icon-loader/icon-loader.vue";
+export default {
+  components: { iconLoader },
+  setup() {
+    const items = ref([
+      {
+        title: "ریحان",
+        groups: ["سوگ", "‌خودشناسی", " روابط عاطفی"],
+      },
+      {
+        title: "ریشه",
+        groups: ["دانشجویان روانشناسی", "روانکاوی"],
+      },
+      {
+        title: "نرجس",
+        groups: ["والدین", " کودکان و نوجوانان"],
+      },
+    ]);
+
+    return { items };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .footer {
-   height: 300px;
-   background-color: var(--color-palette-primary);
+  height: 300px;
+  background-color: var(--color-palette-primary);
 
-   &__main {
-      width: 100%;
-      height: 100%;
-      border-left: 1px solid var(--color-background-main);
-   }
+  &__main {
+    width: 100%;
+    height: 100%;
+    border-left: 1px solid var(--color-background-main);
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr 1fr 1fr;
+    color: var(--color-text-light);
+  }
+
+  &__socials {
+    padding-top: 80px;
+  }
+
+  &__title {
+    @include typography(titre-md);
+  }
+
+  &__social-items {
+    @include flex();
+    gap: 12px;
+    margin-top: 24px;
+    .social {
+      width: 36px;
+      height: 36px;
+      @include flex($align: center, $justify: center);
+      background-color: var(--color-palette-green-600);
+      border-radius: 50%;
+      cursor: pointer;
+    }
+  }
+
+  &__hero {
+    background-color: var(--color-palette-green-500);
+    border-radius: 1000px;
+    @include flex($dir: column, $justify: center);
+  }
+
+  .hero {
+    padding-inline: 70px;
+    &__headline {
+      @include typography(display-sm);
+    }
+    &__sub-headline {
+      @include typography(headline-sm);
+    }
+    &__actions {
+      @include flex();
+      gap: 16px;
+      margin-top: 36px;
+    }
+
+    &__btn {
+      min-width: 155px;
+    }
+  }
+
+  &__groups {
+    padding-top: 80px;
+    > div {
+      margin-right: auto;
+      width: fit-content;
+      margin-left: 16px;
+    }
+    .footer__title {
+      border-bottom: 1px solid var(--color-palette-linenMist);
+      padding-bottom: 8px;
+    }
+  }
+
+  &__group {
+    @include typography(text-lg);
+    margin-top: 12px;
+    cursor: pointer;
+  }
 }
 </style>
