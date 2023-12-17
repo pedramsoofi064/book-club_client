c05b030f20787bf7ff32e96885d975fdfe57dbc8
free4lancer247 <@example.com>
2023-12-17 15:56:42 +0330

    next course

---
+++ src/components/common/icons/clock-icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_412_2980)">
+      <path
+        d="M8.55697 3.68684C13.148 1.78484 18.411 3.96584 20.313 8.55684C22.215 13.1478 20.034 18.4108 15.443 20.3128C10.852 22.2148 5.58897 20.0338 3.68697 15.4428C1.78597 10.8518 3.96597 5.58884 8.55697 3.68684Z"
+        stroke="#B38874"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.718 7.98486V12.6359L15.374 14.8649"
+        stroke="#B38874"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_412_2980">
+        <rect width="24" height="24" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+</template>
--- src/components/view/group/single/next-course.vue
+++ src/components/view/group/single/next-course.vue
@@ -1,9 +1,184 @@
 <template>
-  <div class="single-hero">single hero</div>
+  <div class="next-course">
+    <div class="vert-line"></div>
+    <div class="next-course__main container">
+      <div class="hor-line"></div>
+
+      <div class="next-course__facilitator">
+        <h1 class="next-course__facilitator-title">
+          تسهیل‌گر دوره بعد بوک‌کلاب سوگ
+        </h1>
+        <div class="next-course__facilitator-main">
+          <div class="next-course__facilitator-img">
+            <img src="@/assets/images/rayhan.png" alt="" />
+          </div>
+          <div class="next-course__facilitator-content">
+            <h3 class="next-course__facilitator-name">نام تسهیلگر</h3>
+            <p class="next-course__facilitator-desc">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+              استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در
+              ستون و سطرآنچنان که لازم است
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="next-course__content">
+        <div class="course">
+          <h4 class="course__title">نام کتاب دوره بعد</h4>
+          <p class="course__desc">
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+            استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که لازم است
+          </p>
+          <div class="course__sec">
+            <div class="course__field">
+              <div class="course__field-title">
+                <icon-loader name="clock" />
+                <span class="course__field-text">زمان برگزاری</span>
+              </div>
+              <div class="course__field-value">هر سه‌شنبه، ساعت ۱۷:۳۰</div>
+            </div>
+          </div>
+          <div class="course__sec">
+            <div class="course__field">
+              <div class="course__field-title">
+                <icon-loader name="book-house" />
+                <span class="course__field-text">نحوه برگزاری</span>
+              </div>
+              <div class="course__field-value">
+                <span class="course__field-badge">حضوری</span>
+              </div>
+            </div>
+            <base-button>ثبت نام دوره بعد</base-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.next-course {
+  border-top: 1px solid var(--color-palette-blush-500);
+  position: relative;
+
+  .vert-line {
+    height: 1px;
+    background-color: var(--color-palette-blush-500);
+    position: absolute;
+    left: 0;
+    right: 10%;
+    bottom: 0;
+  }
+  // border-bottom: 1px s olid var(--color-palette-blush-500);
+  &__main {
+    @include flex($align: stretch);
+    padding-top: 50px;
+    position: relative;
+    .hor-line {
+      width: 1px;
+      background-color: var(--color-palette-blush-500);
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: -30px;
+    }
+  }
+
+  &__facilitator {
+    padding: 40px 50px;
+    background-color: var(--color-palette-lightBeige);
+    border-radius: 213px 0px 0px 213px;
+    // flex: 60%;
+    width: 55%;
+  }
+
+  &__facilitator-title {
+    @include typography(headline-md);
+    color: var(--color-palette-blush-600);
+    margin-bottom: 27px;
+  }
+  &__facilitator-main {
+    @include flex();
+    gap: 36px;
+  }
+  &__facilitator-img {
+    min-width: 270px;
+    height: 270px;
+    border-radius: $circle;
+    overflow: hidden;
+    > img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  &__facilitator-content {
+    margin-block: auto;
+  }
+  &__facilitator-name {
+    color: var(--color-text-dark1);
+    @include typography(title-lg);
+    margin-bottom: 12px;
+  }
+  &__facilitator-desc {
+    color: var(--color-text-dark2);
+    @include typography(text-lg);
+    // margin-bottom: 12px;
+    max-width: 380px;
+  }
+
+  &__content {
+    padding: 40px 50px;
+    background-color: var(--color-background-light);
+    border: 1px solid var(--color-palette-darkBeige);
+    border-radius: 48px;
+    flex: 1;
+  }
+
+  .course {
+    margin-block: auto;
+
+    &__title {
+      color: var(--color-text-dark1);
+      @include typography(title-lg);
+      margin-bottom: 12px;
+    }
+    &__desc {
+      color: var(--color-text-dark2);
+      @include typography(text-lg);
+      margin-bottom: 25px;
+    }
+    &__sec {
+      @include flex($justify: space-between);
+      margin-bottom: 25px;
+    }
+    &__field {
+    }
+    &__field-title {
+      @include flex($align: center);
+      gap: 5px;
+      color: var(--color-text-dark1);
+      @include typography(title-md);
+      margin-bottom: 8px;
+    }
+
+    &__field-value {
+      color: var(--color-text-dark2);
+      @include typography(text-lg);
+    }
+    &__field-badge {
+      color: var(--color-text-dark1);
+      @include typography(title-md);
+      padding: 5px 10px;
+      background-color: var(--color-palette-darkBeige);
+      border-radius: 100px;
+    }
+  }
+}
+</style>
